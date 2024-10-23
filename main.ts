914442fd1c87846b978c00bccc63750006f5f4f4
Danilo Just <92367836+jus-dan@users.noreply.github.com>
2024-10-23 16:35:30 +0200

    game ready

--- main.ts
+++ main.ts
@@ -1,68 +1,93 @@
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    init()
-    for (let index = 0; index < 3; index++) {
-        music.play(music.tonePlayable(988, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
-        basic.pause(1000)
-    }
-    music.play(music.tonePlayable(988, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-    go = 1
-    delay = 10
-})
 function initLight () {
     mode = 0
-    strip = neopixel.create(DigitalPin.P14, 60, NeoPixelMode.RGB)
+    streifen1 = neopixel.create(DigitalPin.P14, anzahlLeds, NeoPixelMode.RGB)
     color = neopixel.colors(NeoPixelColors.Purple)
+    delay = 100
 }
 input.onButtonPressed(Button.A, function () {
     initLight()
 })
 input.onPinPressed(TouchPin.P2, function () {
-    if (go == 1) {
-        strip2.rotate(2)
+    if (mode == 0) {
+        color = neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255))
+    } else {
+        if (go == 1) {
+            streifen2.rotate(schrittweite)
+            punkte2 += 1
+        }
     }
 })
-function init () {
-    mode = 1
-    music.setVolume(31)
-    go = 0
-    delay = 100
-    rounds = 0
-    strip = neopixel.create(DigitalPin.P14, 60, NeoPixelMode.RGB)
-    strip2 = neopixel.create(DigitalPin.P14, 60, NeoPixelMode.RGB)
-    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
-    strip2.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-    strip2.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
-}
 input.onButtonPressed(Button.B, function () {
-    color = neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255))
+    initRace()
 })
 input.onPinPressed(TouchPin.P1, function () {
-    if (go == 1) {
-        strip.rotate(2)
+    if (mode == 0) {
+    	
+    } else {
+        if (go == 1) {
+            streifen1.rotate(schrittweite)
+            punkte1 += 1
+        }
     }
 })
+function initRace () {
+    rounds = 3
+    mode = 1
+    go = 0
+    delay = 100
+    schrittweite = 3
+    punkte1 = 0
+    punkte2 = 0
+    streifen1 = neopixel.create(DigitalPin.P14, anzahlLeds, NeoPixelMode.RGB)
+    streifen2 = neopixel.create(DigitalPin.P14, anzahlLeds, NeoPixelMode.RGB)
+    streifen1.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    streifen1.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    streifen1.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    streifen2.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    streifen2.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+    streifen2.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+    for (let index = 0; index < 3; index++) {
+        music.play(music.tonePlayable(988, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+        basic.pause(1000)
+    }
+    music.play(music.tonePlayable(988, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    delay = 10
+    go = 1
+}
 let rounds = 0
-let strip2: neopixel.Strip = null
-let color = 0
-let strip: neopixel.Strip = null
-let mode = 0
-let delay = 0
+let punkte1 = 0
+let punkte2 = 0
+let schrittweite = 0
+let streifen2: neopixel.Strip = null
 let go = 0
+let delay = 0
+let color = 0
+let streifen1: neopixel.Strip = null
+let mode = 0
+let anzahlLeds = 0
+anzahlLeds = 60
+music.setVolume(10)
 initLight()
 basic.forever(function () {
     if (mode == 0) {
-        strip.showColor(color)
-        strip.setBrightness(Math.map(pins.analogReadPin(AnalogPin.P0), 0, 1023, 0, 255))
-        strip.show()
-        basic.pause(delay)
+        streifen1.showColor(color)
+        streifen1.setBrightness(Math.map(pins.analogReadPin(AnalogPin.P0), 0, 1023, 0, 255))
+        streifen1.show()
     }
+    basic.pause(delay)
 })
 basic.forever(function () {
     if (mode == 1) {
-        strip.show()
+        streifen1.show()
         basic.pause(delay)
-        strip2.show()
-        basic.pause(delay)
+        streifen2.show()
+        if (punkte1 * schrittweite >= rounds * anzahlLeds - schrittweite || punkte2 * schrittweite >= rounds * anzahlLeds - schrittweite) {
+            go = 0
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+            delay = 200
+            punkte1 = 0
+            punkte2 = 0
+        }
     }
+    basic.pause(delay)
 })
